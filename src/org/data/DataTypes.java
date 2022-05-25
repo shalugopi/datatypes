852095d4e1b44030db94fc89f1f6257ce0f22902
shalugopi <krisalu10@gmail.com>
2022-05-25 10:45:55 +0530

    new project into git

---
+++ src/org/data/DataTypes.java
@@ -0,0 +1,30 @@
+package org.data;
+
+public class DataTypes {
+public void dataTypes() {
+	System.out.println("Different datatypes");
+}
+public static void main(String[] args) {
+	DataTypes d =new DataTypes();
+	d.dataTypes();
+	byte b=13;
+	System.out.println(b);
+	short s=12345;
+	System.out.println(s);
+	int i=45765484;
+	System.out.println(i);
+	float f= 1654.16541f;
+	System.out.println(f);
+long l=8798469165497l;
+System.out.println(l);
+double c=11684998.1984984;
+System.out.println(c);
+char e='a';
+System.out.println(e);
+String h="aaaaaaa000000;;;;";
+System.out.println(h);
+boolean j=false;
+System.out.println(j);
+Object o=5564824956.54795;
+}
+}
